5e90335a178f50a4273bce17eeaaea684c9fea4e
John DeSilva <desilvjo@umich.edu>
2020-03-28 17:24:20 -0400

    Remove ApplicationJob and ApplicationMailer

--- app/jobs/boring_science/application_job.rb
+++
@@ -1,4 +0,0 @@
-module BoringScience
-  class ApplicationJob < ActiveJob::Base
-  end
-end
--- app/mailers/boring_science/application_mailer.rb
+++
@@ -1,6 +0,0 @@
-module BoringScience
-  class ApplicationMailer < ActionMailer::Base
-    default from: 'from@example.com'
-    layout 'mailer'
-  end
-end
