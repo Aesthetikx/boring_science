5e8d000e9e1241a07827d44c5572fefc17b17ad0
John DeSilva <desilvjo@umich.edu>
2020-03-29 12:28:53 -0400

    Create Blog object with title

--- app/controllers/boring_science/articles_controller.rb
+++ app/controllers/boring_science/articles_controller.rb
@@ -2,6 +2,14 @@ require_dependency 'boring_science/application_controller'
 
 module BoringScience
   class ArticlesController < ApplicationController
+    before_action :set_blog
+
     def index; end
+
+    private
+
+    def set_blog
+      @blog = OpenStruct.new(title: 'Default Blog')
+    end
   end
 end
