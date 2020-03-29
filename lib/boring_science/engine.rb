f29407b2e7213a64c39c617199276027a365ac57
John DeSilva <desilvjo@umich.edu>
2020-03-29 14:51:52 -0400

    Add rails-ujs javascript

--- lib/boring_science/engine.rb
+++ lib/boring_science/engine.rb
@@ -3,5 +3,6 @@ require 'kramdown'
 module BoringScience
   class Engine < ::Rails::Engine
     isolate_namespace BoringScience
+    config.assets.precompile += ['boring_science/application.js']
   end
 end
