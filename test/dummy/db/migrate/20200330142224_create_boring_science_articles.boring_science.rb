0fb219185a3dc48daa42a3a6723730009b538edc
John DeSilva <desilvjo@umich.edu>
2020-03-30 10:26:32 -0400

    Copy migrations to dummy app

---
+++ test/dummy/db/migrate/20200330142224_create_boring_science_articles.boring_science.rb
@@ -0,0 +1,19 @@
+# This migration comes from boring_science (originally 20200329163237)
+class CreateBoringScienceArticles < ActiveRecord::Migration[6.0]
+  def change
+    create_table :boring_science_articles do |t|
+      t.string :blog, null: false
+      t.string :title, null: false
+      t.text :body, null: false
+      t.date :publication_date
+      t.string :slug, null: false
+      t.references :author, polymorphic: true
+
+      t.index :blog
+      t.index :slug, unique: true
+      t.index :author
+
+      t.timestamps
+    end
+  end
+end
