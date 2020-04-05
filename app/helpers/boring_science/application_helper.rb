84b71a055206493b57c24733ce868122c13631c5
John DeSilva <desilvjo@umich.edu>
2020-04-05 12:04:07 -0400

    Add article date helper

--- app/helpers/boring_science/application_helper.rb
+++ app/helpers/boring_science/application_helper.rb
@@ -8,5 +8,15 @@ module BoringScience
       items = [boring_science_article, boring_science_blog]
       items.compact.map(&:title).map(&:presence).compact.join(' - ')
     end
+
+    def boring_science_article_date(article)
+      str = ''
+
+      str += article.publication_date&.strftime('%B %-d, %Y')
+
+      str += ' (Unpublished)' unless article.published?
+
+      str
+    end
   end
 end
